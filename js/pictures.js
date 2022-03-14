c85b02ba60ed54a729926f26b1c6df2cd82141d8
tatiana_kirsanova <alisia7788@mail.ru>
2022-03-14 13:24:39 +0300

    Отрисуй меня полностью (часть 2)

--- js/pictures.js
+++ js/pictures.js
@@ -1,4 +1,5 @@
 import {formsSimilarEntity} from './data.js';
+import {showBigPicture} from './galery.js';
 
 const pictureContainer = document.querySelector('.pictures');
 const pictureTemplate = document.querySelector('#picture').content.querySelector('.picture');
@@ -8,11 +9,13 @@ const picureTemlateRandom = formsSimilarEntity();
 const fragment = document.createDocumentFragment();
 
 
-picureTemlateRandom.forEach(({url,comments,likes}) => {
+picureTemlateRandom.forEach((picture) => {
+  const {url,comments, likes} = picture;
   const pictureItem = pictureTemplate.cloneNode(true);
   pictureItem.querySelector('.picture__img').src = url;
   pictureItem.querySelector('.picture__comments').textContent = comments.length;
   pictureItem.querySelector('.picture__likes').textContent = likes;
+  pictureItem.addEventListener('click', ()=> showBigPicture(picture));
   fragment.appendChild(pictureItem);
 });
 
