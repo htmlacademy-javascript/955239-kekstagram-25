f77f46c9595e774203a7fe74fa4dae5b4fd8bb72
Keks <keks@htmlacademy.ru>
2022-04-03 14:41:24 +0300

    :heavy_check_mark: Сборка #11

---
+++ 11/js/pictures.js
@@ -0,0 +1,24 @@
+import {formsSimilarEntity} from './data.js';
+import {showBigPicture} from './galery.js';
+
+const pictureContainer = document.querySelector('.pictures');
+const pictureTemplate = document.querySelector('#picture').content.querySelector('.picture');
+
+const picureTemlateRandom = formsSimilarEntity();
+
+const fragment = document.createDocumentFragment();
+
+
+picureTemlateRandom.forEach((picture) => {
+  const {url,comments, likes} = picture;
+  const pictureItem = pictureTemplate.cloneNode(true);
+  pictureItem.querySelector('.picture__img').src = url;
+  pictureItem.querySelector('.picture__comments').textContent = comments.length;
+  pictureItem.querySelector('.picture__likes').textContent = likes;
+  pictureItem.addEventListener('click', ()=> showBigPicture(picture));
+  fragment.appendChild(pictureItem);
+});
+
+pictureContainer.appendChild(fragment);
+
+export {pictureContainer, pictureTemplate, picureTemlateRandom, fragment};
